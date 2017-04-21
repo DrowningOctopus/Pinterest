07f2a56bf58fa0fa2b48f77f977130c0008d6334
Manon Véber <manon.veber@etudiant.mines-nantes.fr>
2017-04-21 14:57:16 +0200

    signatures des méthodes de la remote interface

--- PinterestRMI/src/rmi/RemoteInterface.java
+++ PinterestRMI/src/rmi/RemoteInterface.java
@@ -2,18 +2,32 @@ package rmi;
 
 import java.rmi.Remote;
 
+import pinterest.Epingle;
 import pinterest.Tableau;
+import utilisateurs.Ustandard;
 
 public interface RemoteInterface extends Remote {
 	//() throws RemoteException;
 	
 	public void validerCreationTableau(Tableau t);
 	
+	public void validerModificationTableau(Tableau t, String s);
+	
+	public void validerPartageTableau(Tableau t, Ustandard u);
+		
+	
+	public void validerCreationEpingle(Epingle e);
+	
+	public void validerAjoutEpingle(Epingle e, Tableau t);
+	
+	public void validerSuppressionEpingle(Epingle e, Tableau t);
+	
 	/*
 	// TODO
 	creerTableau()
 	modifierTableau(Tableau)
 	partagerTableau(Tableau)
+	
 	
 	// TODO
 	creerEpingle()
