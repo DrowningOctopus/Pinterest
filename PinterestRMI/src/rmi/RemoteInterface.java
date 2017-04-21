4e2b1ae1b411109cbb8feea568482c51bec95e7a
ManetteS <Manette@172.17.3.70>
2017-04-21 14:49:17 +0200

    Note sur les méthodes à placer dans la RemoteInterface

--- PinterestRMI/src/rmi/RemoteInterface.java
+++ PinterestRMI/src/rmi/RemoteInterface.java
@@ -4,4 +4,27 @@ import java.rmi.Remote;
 
 public interface RemoteInterface extends Remote {
 	//() throws RemoteException;
+	
+	/*
+	// TODO
+	creerTableau()
+	modifierTableau(Tableau)
+	partagerTableau(Tableau)
+	
+	// TODO
+	creerEpingle()
+	ajouterEpingle(Epingle, Tableau)
+	supprimerEpingle(Epingle, Tableau)
+	
+	// Plus tard
+	parcourirFil ?
+	
+	// Plus tard
+	ecrireMessage(Ustandard)
+	recevoirMessage(Utilisateur)
+	recevoirNotifLu(Utilisateur)
+	
+	// Side note
+	main = agir
+	*/
 }
