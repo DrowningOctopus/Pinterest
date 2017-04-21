aefb74eadb6d5e4e3f83574c18246f5f6b486a06
ManetteS <Manette@172.17.3.70>
2017-04-21 15:00:57 +0200

    Copié-collé du tutoriel pour créer RemoteClient

---
+++ PinterestRMI/src/rmi/RemoteClient.java
@@ -0,0 +1,22 @@
+package rmi;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RemoteClient {
+	private RemoteClient() {}
+
+    public static void main(String[] args) {
+
+        String host = (args.length < 1) ? null : args[0];
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            RemoteInterface stub = (RemoteInterface) registry.lookup("RemoteInterface");
+            //String response = stub.sayHello();
+            //System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
