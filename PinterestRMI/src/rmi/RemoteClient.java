7c3cbff6c46d6e1d6b3859ee8195258631fc4f80
ManetteS <Manette@172.17.3.70>
2017-04-21 15:53:35 +0200

    Ajout de l'attribut client dans RemoteClient.

--- PinterestRMI/src/rmi/RemoteClient.java
+++ PinterestRMI/src/rmi/RemoteClient.java
@@ -3,16 +3,27 @@ package rmi;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import utilisateurs.Ustandard;
+
 public class RemoteClient {
+	private static Ustandard client;
+	
 	private RemoteClient() {}
 
+	private static void setClient(RemoteInterface ri) {
+		
+	}
+	
     public static void main(String[] args) {
 
         String host = (args.length < 1) ? null : args[0];
         try {
             Registry registry = LocateRegistry.getRegistry(host);
             RemoteInterface stub = (RemoteInterface) registry.lookup("RemoteInterface");
+            
             System.out.println(stub.check());
+            
+            setClient(stub);
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
