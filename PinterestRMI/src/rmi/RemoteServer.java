6244da041a4955be3a6ce0c32b00a2efc73fb8e7
ManetteS <Manette@172.17.3.70>
2017-04-21 14:58:40 +0200

    Auto-génération des squelettes des méthodes de RemoteServer venant de RemoteInterface.

--- PinterestRMI/src/rmi/RemoteServer.java
+++ PinterestRMI/src/rmi/RemoteServer.java
@@ -4,8 +4,9 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-import example.hello.Hello;
-import example.hello.Server;
+import pinterest.Epingle;
+import pinterest.Tableau;
+import utilisateurs.Ustandard;
 
 public class RemoteServer implements RemoteInterface {
 	public RemoteServer() {
@@ -24,4 +25,40 @@ public class RemoteServer implements RemoteInterface {
             e.printStackTrace();
         }
     }
+
+	@Override
+	public void validerCreationTableau(Tableau t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validerModificationTableau(Tableau t, String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validerPartageTableau(Tableau t, Ustandard u) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validerCreationEpingle(Epingle e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validerAjoutEpingle(Epingle e, Tableau t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validerSuppressionEpingle(Epingle e, Tableau t) {
+		// TODO Auto-generated method stub
+		
+	}
 }
